37b70b2812b2767cb3e6175d8f2576174f5f8ed3
Dan O'Malley <dan.omalley@amag.com>
2024-03-04 11:57:08 -0500

    fix for localsymmetry integration

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-to start dev server,
-kill the docker container (as port is in use)
+to start dev server
+run: npm run start
 
-then run
 
-npm run dev
+in order to access session, add this to
+root.web/Caddyfile.dockerforwindows
+
+route /newapp/* {
+	uri strip_prefix /newapp
+	reverse_proxy http://host.docker.internal:7999
+}
+
+then, once logged in to localsymmetry.net, go to
+
+https://localsymmetry.net/newapp/
--- src/app/models.ts
+++ src/app/models.ts
@@ -8,3 +8,40 @@ export interface CustomThemeConfiguration {
 export interface IDMConfiguration {
   CustomThemeConfiguration: CustomThemeConfiguration
 }
+
+export interface Employee {
+  BuildingId: string
+  BuildingName: string
+  CompanyId: string
+  CompanyName: string
+  Emails: any[]
+  EmployeeId: string
+  EmployeeNumber: string
+  EmployeeType: string
+  EmployeeTypeId: number
+  Expired: boolean
+  FirstName: string
+  Gender: string
+  HasWebUserAccess: boolean
+  PhotoUrl: string
+  PrimaryEmail: string
+  Suspended: boolean
+  Terminated: boolean
+  UniqueIdentifier: string
+
+}
+
+export interface Identity {
+  Id: string
+  FirstName: string
+  LastName: string
+
+}
+
+export interface Credential {
+  ActivationDate: string
+  CredentialFormat: number
+  Id: string
+  HotStamp: string
+  RawNumber: number
+}
